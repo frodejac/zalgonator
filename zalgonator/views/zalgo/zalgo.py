470b5573d4307a1dfc05cfd1ca5fc6ff86da0b13
Frode Jacobsen <fj@itverket.no>
2019-08-16 09:15:40 +0200

    Improve on the POC

--- zalgonator/views/zalgo/zalgo.py
+++ zalgonator/views/zalgo/zalgo.py
@@ -1,8 +1,11 @@
+from typing import Any
+
 from flask import Blueprint, make_response, render_template
 from flask.wrappers import Response
 
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField, BooleanField, IntegerField
+from werkzeug.datastructures import ImmutableMultiDict
+from wtforms import TextAreaField, SubmitField, BooleanField, IntegerField, Field
 
 from zalgonator.zalgo.zalgonate import zalgonate
 
@@ -10,25 +13,43 @@ zalgo = Blueprint('zalgo', __name__)
 
 
 class TextAreaForm(FlaskForm):
-    textarea = TextAreaField('Enter text here:')
-    rng_range_low = IntegerField('Minimum number of diacritics per character', default=4)
-    rng_range_high = IntegerField('Maximum number of diacritics per character', default=50)
-    rng_check = BooleanField('Randomize zalgonation')
+    textarea = TextAreaField('Enter text here:', render_kw={'rows': 10, 'cols': 60})
+    rng_range_low = IntegerField('Min. diacritics/char', default=4, render_kw={'cols':4})
+    rng_range_high = IntegerField('Max. diacritics/char', default=50)
+    rng_select = BooleanField('Randomize')
+    bold_select = BooleanField('Bold')
     submit = SubmitField('Zalgonate')
 
 
 @zalgo.route('/', methods=['GET', 'POST'])
 def index() -> Response:
+    def full_field_process(field: Field, val: Any):
+        field.process(ImmutableMultiDict([(field.short_name, str(val))]))
+
+    def fix_ranges(form_: TextAreaForm):
+        lo = form_.rng_range_low.data
+        hi = form_.rng_range_high.data
+        if lo > 1000:
+            lo = 1000
+        if lo < 0:
+            lo = 0
+        if lo > hi:
+            hi = lo
+        full_field_process(form_.rng_range_low, lo)
+        full_field_process(form_.rng_range_high, hi)
+
     form = TextAreaForm()
     if form.validate_on_submit():
+        fix_ranges(form)
         return render_template(
             'zalgo/index.html',
             form=form,
             zalgotext=zalgonate(
-                form.textarea.data,
-                form.rng_check.data,
-                form.rng_range_low.data,
-                form.rng_range_high.data
+                text=form.textarea.data,
+                rng=form.rng_select.data,
+                range_low=form.rng_range_low.data,
+                range_high=form.rng_range_high.data,
+                bold=form.bold_select.data
             )
         )
     return render_template(
--- zalgonator/zalgo/zalgonate.py
+++ zalgonator/zalgo/zalgonate.py
@@ -1,4 +1,4 @@
-from html import escape, unescape
+import html
 
 import random
 import re
@@ -6,20 +6,37 @@ import re
 ws_pattern = re.compile(r'\s+', flags=re.UNICODE)
 
 
-def zalgonate(text: str, rng: bool = True, range_low: int = 4, range_high: int = 50) -> str:
+def zalgonate(text: str, rng: bool = True, range_low: int = 4, range_high: int = 50, bold: bool = False) -> str:
     if not rng:
         random.seed(text)
+
     zalgotext = ""
+
     for c in text:
-        zalgotext += add_diacritics(c, range_low, range_high)
-    return escape(zalgotext)
+        zalgotext += add_diacritics(c, range_low, range_high, bold)
+
+    return html_escape(zalgotext)
 
 
-def add_diacritics(c: str, lo: int, hi: int) -> str:
+def add_diacritics(c: str, lo: int, hi: int, bold: bool = False) -> str:
     if ws_pattern.sub('', c):
+        if bold:
+            c = boldify(c)
         return c + ''.join([get_random_diacritic() for _ in range(random.randint(lo, hi))])
     return c
 
 
 def get_random_diacritic() -> str:
     return chr(random.randint(768, 879))
+
+
+def html_escape(text: str):
+    return html.escape(text).replace('\n', '<br/>')
+
+
+def boldify(c: str) -> str:
+    if 65 <= ord(c) <= 90:
+        return chr(ord(c) + 0x1d3bf)
+    if 97 <= ord(c) <= 122:
+        return chr(ord(c) + 0x1d3b9)
+    return c
